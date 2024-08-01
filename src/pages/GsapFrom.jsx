21c566d461b727aaaeddad3891c024adffc01ad9
adiiittt <avyas8927@mgmail.com>
2024-08-01 19:40:01 +0530

    GSAP To, From, FromTo, Timeline

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
-  // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 440,
+      y: 200,
+      repeat: -1,
+      yoyo: true,
+      rotation: 250,
+      duration: 2,
+      ease: 'bounce.inOut'
+    });
+  });
 
   return (
     <main>
--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,6 +1,27 @@
-const GsapFromTo = () => {
-  // TODO: Implement the gsap.fromTo() method
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
+const GsapFromTo = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      "#red-box",
+      {
+        x: 0,
+        rotation: 0,
+        borderRadius: "0%",
+      },
+      {
+        x: 800,
+        // y: 200,
+        repeat: -1,
+        yoyo: true,
+        borderRadius: "100%",
+        rotation: 360,
+        duration: 2,
+        ease: "bounce",
+      }
+    );
+  });
   return (
     <main>
       <h1>GsapFromTo</h1>
